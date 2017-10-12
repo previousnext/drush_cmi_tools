60705847bcbfa2c14249440b48895e74f3c91968
Kim Pepper <kim@pepper.id.au>
2017-10-12 20:30:05 +1100

    Fix code style

--- tests/Unit/DrushCmiToolsCommandsTest.php
+++ tests/Unit/DrushCmiToolsCommandsTest.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\drush_cmi_tools\Tests\Unit;
 
-use Drupal\drush_cmi_tools\Commands\DrushCmiToolsCommands;
-
 /**
  * Provides a DrushCmiToolsCommandsTest.
  *
@@ -15,7 +13,7 @@ class DrushCmiToolsCommandsTest extends \PHPUnit_Framework_TestCase {
    * Tests the constructor.
    */
   public function testCreate() {
-//    $commands = new DrushCmiToolsCommands(NULL, NULL, NULL, NULL, NULL, NULL, NULL);
     $this->assertTrue(TRUE);
   }
+
 }
