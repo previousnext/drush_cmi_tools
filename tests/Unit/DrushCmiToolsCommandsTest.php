14f0c3060d13f8d40ed5a2a1bb48a678bc60bd2d
Kim Pepper <kim@pepper.id.au>
2017-10-12 20:27:52 +1100

    Adds a test

---
+++ tests/Unit/DrushCmiToolsCommandsTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\drush_cmi_tools\Tests\Unit;
+
+use Drupal\drush_cmi_tools\Commands\DrushCmiToolsCommands;
+
+/**
+ * Provides a DrushCmiToolsCommandsTest.
+ *
+ * @coversDefaultClass \Drupal\drush_cmi_tools\Commands\DrushCmiToolsCommands
+ */
+class DrushCmiToolsCommandsTest extends \PHPUnit_Framework_TestCase {
+
+  /**
+   * Tests the constructor.
+   */
+  public function testCreate() {
+//    $commands = new DrushCmiToolsCommands(NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+    $this->assertTrue(TRUE);
+  }
+}
