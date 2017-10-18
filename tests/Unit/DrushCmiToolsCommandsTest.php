90fc248b80ba0f57ca629ce3d44cf317da2d8dac
Kim Pepper <kim@pepper.id.au>
2017-10-18 15:33:13 +1100

    Port commands to Drush 9

---
+++ tests/Unit/DrushCmiToolsCommandsTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\drush_cmi_tools\Tests\Unit;
+
+/**
+ * Provides a DrushCmiToolsCommandsTest.
+ *
+ * @coversDefaultClass \Drupal\drush_cmi_tools\Commands\DrushCmiToolsCommands
+ */
+class DrushCmiToolsCommandsTest extends \PHPUnit_Framework_TestCase {
+
+  /**
+   * Tests the constructor.
+   */
+  public function testCreate() {
+    $this->markTestIncomplete();
+  }
+
+}
