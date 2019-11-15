3bc153482bdd5d53691d7e25b9332bbe5cb439b3
Kim <kim@pepper.id.au>
2019-11-15 12:34:10 +1100

    Adds drush 10 support

--- tests/Unit/DrushCmiToolsCommandsTest.php
+++ tests/Unit/DrushCmiToolsCommandsTest.php
@@ -2,12 +2,14 @@
 
 namespace Drupal\drush_cmi_tools\Tests\Unit;
 
+use PHPUnit\Framework\TestCase;
+
 /**
  * Provides a DrushCmiToolsCommandsTest.
  *
  * @coversDefaultClass \Drupal\drush_cmi_tools\Commands\DrushCmiToolsCommands
  */
-class DrushCmiToolsCommandsTest extends \PHPUnit_Framework_TestCase {
+class DrushCmiToolsCommandsTest extends TestCase {
 
   /**
    * Tests the constructor.
